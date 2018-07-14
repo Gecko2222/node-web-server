916873ec7ff2f424963c58bbd2cf8d9f77cd867f
Lucian <lucian.jakob@stud.fh-rosenheim.de>
2018-07-14 16:32:47 +0200

    test

--- server.js
+++ server.js
@@ -52,8 +52,13 @@ app.get('/about', (req, res) => {
 res.render('about.hbs', {
   pageTitle: 'About Page',
 });
+});
 
-})
+app.get('/projects', (req, res) => {
+res.render('projects.hbs', {
+  pageTitle: 'Projects Page',
+});
+});
 
 app.get('/bad', (req, res) => {
   res.send({
